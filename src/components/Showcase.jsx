0c23ea656766e93cffd71e233a35312875d602db
Yusuf Toyin K <ytoyin67@yahoo.com>
2023-10-24 22:51:00 +0100

    first commit

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Showcase(props) {
+  return (
+    <div  >
+      <h1>Introduction</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta laudantium quae id, numquam minima, totam quos in, repudiandae ipsum nisi unde voluptatum et suscipit. Cumque  non, quas cupiditate eos reprehenderit magni, inventore ut tempora, ducimus rem officiis saepe expedita ipsa.</p>
+    </div>
+  )
+}
+
+export default Showcase
