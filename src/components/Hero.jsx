109c57332ff656d468278547072387789da913d5
Yusuf Toyin K <ytoyin67@yahoo.com>
2023-10-25 12:19:43 +0100

    logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@
 function Hero() {
   return (
     <div className="hero py-3">
-        <div className="bgBlue" style={{height:'110px', width: '130px', borderRadius: '50%', position: 'absolute', top: "-70px", right: '100px'}}/>
-        <div className="d-flex align-items-center pt-3" style={{paddingLeft:"60px"}}>
-          <span style={{backgroundColor: "#0fd2a8", width: "110px", height: '110px', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <div className="bgBlue" style={{height:'110px', width: '130px', borderRadius: '50%', position: 'absolute', top: "-90px", right: '100px'}}/>
+        <div className="d-flex align-items-center pt-5" style={{paddingLeft:"28px"}}>
+          <span style={{backgroundColor: "#0fd2a8", width: "110px", height: '110px', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center',paddingRight: '10px'}}>
 
               <span style={{fontSize: '3.5rem', fontWeight: "bold", color: 'white', textShadow: '2px 2px 2px black'}}>S</span>
           </span>  
