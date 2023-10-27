64ccf4af152f3143d48d09a2f6c509de8052b482
Yusuf Toyin K <ytoyin67@yahoo.com>
2023-10-27 17:16:04 +0100

    mobile hero sec

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@
 function Hero() {
   return (
     <div className="hero pt-3 pb-4">
-        <div className="bgBlue" style={{height:'130px', width: '210px', borderRadius: '50%', position: 'absolute', top: "-80px", right: '100px'}}/>
-        <div className="d-flex align-items-center pt-5 ps-md-5 max-width">
+        <div className="bgBlue top-circle" />
+        <div className="d-flex align-items-center pt-5 ps-md-4 max-width mobile-pad ">
           <span style={{backgroundColor: "#0fd2a8", width: "110px", height: '110px', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center',paddingRight: '10px', marginLeft: '28px'}}>
 
               <span style={{fontSize: '3.5rem', fontWeight: "bold", color: 'white', textShadow: '2px 2px 2px black'}}>S</span>
@@ -18,20 +18,20 @@ function Hero() {
           </h1>
         </div>
 
-        <div className="grid p-3 mt-4 max-width mx-auto">
-            <div className="largest">
-                  <h6 className="blueText">
+        <div className="grid p-3 mt-4 max-width mx-auto hero-container">
+            <div className="largest ">
+                  <h4 className="blueText text-center mobile-center">
                     WE ARE HERE FOR
-                  </h6>
-                  <h1 className="blueText bold-h1 hero-h1">
+                  </h4>
+                  <h1 className="blueText bold-h1 hero-h1 mobile-center ">
                     THE LARGEST BUSINESS EXPERT
                   </h1>
                 
-                  <p className="text-dark pt-4 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis deserunt dicta vitae necessitatibus, nostrum placeat tempora ad minus mollitia!</p>
+                  <p className="text-dark pt-4 pb-3 mobile-center" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis deserunt dicta vitae necessitatibus, nostrum placeat tempora ad minus mollitia!</p>
 
-                  <div className="d-flex gap-4 mt-5">
+                  <div className="d-sm-flex gap-4 mt-5 flex-col">
                       <button className="btn btn-default bgGreen text-light py-2 px-4 border-radius">Get Started</button>
-                      <button className="btn btn-default bgBlue text-light py-2 px-sm-4 border-radius ">Explore More</button>
+                      <button className="btn btn-default  bgBlue text-light py-2 px-sm-4 border-radius">Explore More</button>
                   </div>
             </div>
 
