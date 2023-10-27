1593ba12508f09b15df09a20dc6d536bcaffc0b7
Yusuf Toyin K <ytoyin67@yahoo.com>
2023-10-27 14:07:45 +0100

    mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 
 // import logo from '../images/logo.jpg'
+ import woman from '../images/image4.jpg'
 
 function Hero() {
   return (
-    <div className="hero py-3">
-        <div className="bgBlue" style={{height:'110px', width: '150px', borderRadius: '50%', position: 'absolute', top: "-85px", right: '100px'}}/>
-        <div className="d-flex align-items-center pt-4 ps-md-5 max-width">
+    <div className="hero pt-3 pb-4">
+        <div className="bgBlue" style={{height:'130px', width: '210px', borderRadius: '50%', position: 'absolute', top: "-80px", right: '100px'}}/>
+        <div className="d-flex align-items-center pt-5 ps-md-5 max-width">
           <span style={{backgroundColor: "#0fd2a8", width: "110px", height: '110px', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center',paddingRight: '10px', marginLeft: '28px'}}>
 
               <span style={{fontSize: '3.5rem', fontWeight: "bold", color: 'white', textShadow: '2px 2px 2px black'}}>S</span>
@@ -17,22 +18,28 @@ function Hero() {
           </h1>
         </div>
 
-        <div className="grid p-3 pt-5 max-width mx-auto">
-            <div className="p-3">
+        <div className="grid p-3 mt-4 max-width mx-auto">
+            <div className="largest">
                   <h6 className="blueText">
                     WE ARE HERE FOR
                   </h6>
-                  <h1 className="blueText bold-h1">
-                    THE LARGEST
+                  <h1 className="blueText bold-h1 hero-h1">
+                    THE LARGEST BUSINESS EXPERT
                   </h1>
-                  <h1 className="blueText bold-h1">
-                    BUSINESS EXPERT
-                  </h1>
-                  <p className="text-dark w-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis deserunt dicta vitae necessitatibus, nostrum placeat tempora ad minus mollitia!</p>
+                
+                  <p className="text-dark pt-4 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis deserunt dicta vitae necessitatibus, nostrum placeat tempora ad minus mollitia!</p>
+
+                  <div className="d-flex gap-4 mt-5">
+                      <button className="btn btn-default bgGreen text-light py-2 px-4 border-radius">Get Started</button>
+                      <button className="btn btn-default bgBlue text-light py-2 px-sm-4 border-radius ">Explore More</button>
+                  </div>
             </div>
-            <div className="p-3 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse, dolores odit fugit ipsum eum odio impedit quae. Ullam, sapiente?
+
+            <div className="image  p-3">
+           
+               <img src={woman} alt="woman" className='  woman'/>
             </div>
+            
         </div>
     </div>
   )
